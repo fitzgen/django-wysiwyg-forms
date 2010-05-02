b5a9621d9df0a9f5b0df64a97d7225bedeb5f877
Nick Fitzgerald <fitzgen@gmail.com>
2010-05-02 14:40:53 -0700

    Adding basic first draft at the javascript in the WYSIWYG editor

---
+++ wysiwyg_forms/media/wysiwyg_forms.js
@@ -0,0 +1,139 @@
+var DjangoWysiwygFormEditor = (function (exports) {
+
+    /**
+     * Utilities
+     */
+                                   
+    // Updates obj with only the things from other that are not already present
+    // in obj.
+    var complete = function (obj, other) {
+        var thing;
+        for (thing in other)
+            if (other.hasOwnProperty(thing) && !obj.hasOwnProperty(thing))
+                obj[thing] = other[thing];
+        return obj;
+    };
+
+    var map = function (arr, fn) {
+        var results, i;
+        
+        if (typeof Array.prototype.map === "function") {
+            return Array.prototype.map.call(arr, fn);
+        }
+        else {
+            results = [];
+            for (i = 0; i < arr.length; i++)
+                results.push(fn.call(arr[i], arr[i], i));
+            return results;
+        }
+    };
+
+    /**
+     * Fields
+     */
+                                   
+    var Field = function (type, widget) {
+        if (!(this instanceof Field)) {
+            return new Field();
+        }
+        var self = this;
+
+        // Privates
+
+        // Publics
+
+        self.toHtml = function () {
+            return $.tempest("wysiwyg-form-field", complete({
+                widget: (new djangoWysiwygWidgets[self.widget]()).render(
+                    name,
+                    { disabled: "disabled" },
+                    self.choices
+                )
+            }, self));
+        };
+
+        self.type_ = type;
+        self.widget = widget;
+        self.name_ = "Some Field";
+        self.label = "This is some form field.";
+        self.choices = [];
+
+        self.toJson = function () {
+            return JSON.stringify(self);
+        };
+                
+        return self;
+    };
+
+    /**
+     * DjangoWysiwygFormEditor 
+     */
+
+    return function (opts) {
+        if (!(this instanceof DjangoWysiwygFormEditor)) {
+            return new (exports.DjangoWysiwygFormEditor(opts));
+        }
+
+        opts = complete(opts, {
+            name: "Your Form",
+            description: "This is your form."
+        });
+
+        if (opts.target === undefined)
+            throw new TypeError("DjangoWysiwygFormEditor: No target for attachment specified");
+
+        var self = this;
+
+        // Private methods and slots
+
+        var fields = [];
+
+        var formToHtml = function () {
+            return $.tempest("wysiwyg-form", complete({
+                fields: map(fields, function () {
+                    return this.toHtml();
+                }).join("\n")
+            }, self));
+        };
+        
+        var init = function () {
+            $(opts.target).append(formToHtml());
+
+            $(opts.target + " ul.wysiwyg-form-fields").sortable({
+                placeholder: "wysiwyg-form-field-placeholder"
+            });
+        };
+
+        // Public methods and slots
+
+        self.name = opts.name;
+        self.description = opts.description;
+
+        self.newField = function () {
+            // TODO: Ability to create things other than text inputs.
+            var f = new Field("CharField", "TextInput");
+            fields.push(f);
+            $(opts.target + " ul.wysiwyg-form-fields").append(f.toHtml());
+            return this;
+        };
+
+        self.save = function () {
+            throw new Error("TODO");
+        };
+        
+        self.toJson = function () {
+            return JSON.stringify({
+                name: self.name,
+                description: self.description,
+                fields: map(fields, function (f) {
+                    return f.toJson();
+                })
+            });
+        };
+
+        init();
+
+        return self;
+    };
+
+}());
