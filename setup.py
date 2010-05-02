d125dc611e307d55ed1f2b74fa8c7a162b0197fb
Nick Fitzgerald <fitzgen@gmail.com>
2010-05-01 17:09:28 -0700

    Adding license and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="Django WYSIWYG Forms",
+      version="0.1",
+      description="WYSIWYG Form editor/creator django app",
+      author="Nick Fitzgerald",
+      author_email="fitzgen@gmail.com",
+      url="http://github.com/fitzgen/djang-wysiwyg-forms",
+      packages=["wysiwyg_forms"],
+      license="MIT")
