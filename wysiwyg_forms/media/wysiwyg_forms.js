8223fd363445a473e5307dcb13bcbaf0850003b9
Nick Fitzgerald <fitzgen@gmail.com>
2010-05-02 18:33:31 -0700

    Making the form name and description editable

--- wysiwyg_forms/media/wysiwyg_forms.js
+++ wysiwyg_forms/media/wysiwyg_forms.js
@@ -3,7 +3,7 @@ var DjangoWysiwygFormEditor = (function (exports) {
     /**
      * Utilities
      */
-                                   
+
     // Updates obj with only the things from other that are not already present
     // in obj.
     var complete = function (obj, other) {
@@ -16,7 +16,7 @@ var DjangoWysiwygFormEditor = (function (exports) {
 
     var map = function (arr, fn) {
         var results, i;
-        
+
         if (typeof Array.prototype.map === "function") {
             return Array.prototype.map.call(arr, fn);
         }
@@ -31,7 +31,7 @@ var DjangoWysiwygFormEditor = (function (exports) {
     /**
      * Fields
      */
-                                   
+
     var Field = function (type, widget) {
         if (!(this instanceof Field)) {
             return new Field();
@@ -61,12 +61,12 @@ var DjangoWysiwygFormEditor = (function (exports) {
         self.toJson = function () {
             return JSON.stringify(self);
         };
-                
+
         return self;
     };
 
     /**
-     * DjangoWysiwygFormEditor 
+     * DjangoWysiwygFormEditor
      */
 
     return function (opts) {
@@ -95,12 +95,28 @@ var DjangoWysiwygFormEditor = (function (exports) {
                 }).join("\n")
             }, self));
         };
-        
-        var init = function () {
-            $(opts.target).append(formToHtml());
 
-            $(opts.target + " ul.wysiwyg-form-fields").sortable({
-                placeholder: "wysiwyg-form-field-placeholder"
+        var init = function () {
+            var base = $(opts.target);
+            base.append(formToHtml());
+
+            base.find(".wysiwyg-form-controls").tabs();
+
+            base.find("ul.wysiwyg-form-fields").sortable({
+                placeholder: ".wysiwyg-form-field-placeholder"
+            });
+
+            // TODO: make this type of thing a higher order function.
+            var formName = base.find("h1.wysiwyg-form-name");
+            base.find("input.form-name").bind("keyup", function (event) {
+                self.name = $(this).val();
+                formName.text(self.name);
+            });
+
+            var formDescription = base.find("p.wysiwyg-form-description");
+            base.find("input.form-description").bind("keyup", function (event) {
+                self.description = $(this).val();
+                formDescription.text(self.description);
             });
         };
 
@@ -120,7 +136,7 @@ var DjangoWysiwygFormEditor = (function (exports) {
         self.save = function () {
             throw new Error("TODO");
         };
-        
+
         self.toJson = function () {
             return JSON.stringify({
                 name: self.name,
