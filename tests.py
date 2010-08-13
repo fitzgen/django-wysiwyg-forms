4ab0beb5355d89f90a72efe5322e165e9c16a7b0
Nick Fitzgerald <fitzgen@gmail.com>
2010-08-13 11:47:30 -0700

    Adding tests for adding duplicate fields and removing non-existant fields

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .exceptions import (ChoiceDoesNotExist, ChoiceAlreadyExists,
+                         FieldDoesNotExist, FieldAlreadyExists)
 from .models import Form, Field, Choice
 
 class BaseTestCase(TestCase):
@@ -17,6 +19,17 @@ class FormModelTestCase(BaseTestCase):
         self.assertTrue(isinstance(self.form.fields[0], Field),
                         "The new field should be an instance of Field.")
 
+    def test_add_existing_field(self):
+        self.form.add_field("Email")
+        try:
+            self.form.add_field("Email")
+        except FieldAlreadyExists:
+            self.assertTrue(True,
+                            "Adding a field that already exists should throw an error.")
+        else:
+            self.assertTrue(False,
+                            "Adding a field that already exists should throw an error.")
+
     def test_remove_field(self):
         self.form.add_field("Email", type="EmailField")
         self.assertEqual(len(self.form.fields), 1,
@@ -25,6 +38,15 @@ class FormModelTestCase(BaseTestCase):
         self.assertEqual(len(self.form.fields), 0,
                          "Should now have 0 fields")
 
+    def test_remove_nonexistant_field(self):
+        try:
+            self.form.remove_field("non-existant field")
+        except FieldDoesNotExist:
+            self.assertTrue(True,
+                            "Removing non-existant field throws an error")
+        else:
+            self.assertTrue(False,
+                            "Removing non-existant field throws an error")
+
 class FieldModleTestCase(BaseTestCase):
     pass
-
