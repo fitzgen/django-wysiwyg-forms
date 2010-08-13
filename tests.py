7b17f77a93bf3adf31e955b5f96b00dc99ae86c0
Nick Fitzgerald <fitzgen@gmail.com>
2010-08-13 11:58:39 -0700

    Adding tests for add/remove choices from fields

--- tests.py
+++ tests.py
@@ -48,5 +48,47 @@ class FormModelTestCase(BaseTestCase):
             self.assertTrue(False,
                             "Removing non-existant field throws an error")
 
-class FieldModleTestCase(BaseTestCase):
-    pass
+class FieldModelTestCase(BaseTestCase):
+    def setUp(self):
+        super(FieldModelTestCase, self).setUp()
+        self.field = self.form.add_field("Happy and you know it?",
+                                         type="MultipleChoiceField")
+
+    def test_add_choice(self):
+        self.assertEqual(len(self.field.choices), 0,
+                         "Should start with 0 choices")
+        self.field.add_choice("Yes")
+        self.assertEqual(len(self.field.choices), 1,
+                         "Should now have 1 choice")
+        self.assertTrue(isinstance(self.field.choices[0], Choice),
+                        "The new choice should be an instance of Choice.")
+
+    def test_add_existing_choice(self):
+        self.field.add_choice("Yes")
+        try:
+            self.field.add_choice("Yes")
+        except ChoiceAlreadyExists:
+            self.assertTrue(True,
+                            "Adding a choice that already exists should throw an error.")
+        else:
+            self.assertTrue(False,
+                            "Adding a choice that already exists should throw an error.")
+
+    def test_remove_choice(self):
+        self.field.add_choice("No")
+        self.assertEqual(len(self.field.choices), 1,
+                         "Should have 1 choice")
+        self.field.remove_choice("No")
+        self.assertEqual(len(self.field.choices), 0,
+                         "Should now have 0 fields")
+
+    def test_remove_nonexistant_choice(self):
+        try:
+            self.field.remove_choice("non-existant choice")
+        except ChoiceDoesNotExist:
+            self.assertTrue(True,
+                            "Removing non-existant choice throws an error")
+        else:
+            self.assertTrue(False,
+                            "Removing non-existant choice throws an error")
+
