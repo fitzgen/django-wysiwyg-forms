94ae661a6a609da076a4ad9db75b09897a1bd92e
Nick Fitzgerald <fitzgen@gmail.com>
2010-08-24 14:29:49 -0700

    Adding hashchange to the urls as a direct to template view

--- urls.py
+++ urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 from .views import ApplyTransactions, Edit
 
 urlpatterns = patterns(
     '',
     url(r"^save/$", ApplyTransactions, name="wysiwyg_forms_apply_transactions"),
-    url(r"^/$", Edit, name="wysiwyg_forms_new_form"),
+    url(r"^$", Edit, name="wysiwyg_forms_new_form"),
     url(r"^(\d+)/$", Edit, name="wysiwyg_forms_edit_form"),
+
+    # This file cannot be {% included %} because it contains "</script>" in a
+    # string which breaks the rest of the JS.
+    url(r"media/jquery.ba-hashchange.js",
+        direct_to_template,
+        { "template" : "wysiwyg_forms/jquery.ba-hashchange.min.js",
+          "mimetype" : "text/javascript" },
+        name="wysiwyg_forms_media_hashchange_js"),
 )
