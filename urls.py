ff780d6043cb7cf2fbd62aaca17799d380dd4d03
Nick Fitzgerald <fitzgen@gmail.com>
2010-08-24 11:34:48 -0700

    Adding the Edit view class which handles both new forms and existing

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 
-from .views import ApplyTransactions
+from .views import ApplyTransactions, Edit
 
 urlpatterns = patterns(
     '',
     url(r"^save/$", ApplyTransactions, name="wysiwyg_forms_apply_transactions"),
+    url(r"^/$", Edit, name="wysiwyg_forms_new_form"),
+    url(r"^(\d+)/$", Edit, name="wysiwyg_forms_edit_form"),
 )
--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 from django import http
 from django import template
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext
 from django.utils import simplejson as json
 
 from .models import Form
@@ -33,6 +35,9 @@ class WysiwygFormView(object):
     def get_template(self):
         return template.loader.get_template(self.template_name)
 
+    def get_mimetype(self, request, *args, **kwargs):
+        return self.mimetype
+
     def has_permissions(self, request, *args, **kwargs):
         """
         Subclass and overload this method for custom authorization.
@@ -47,11 +52,10 @@ def deunicode(d):
         d[str(k)] = d.pop(k)
 
 class ApplyTransactions(WysiwygFormView):
+    mimetype = "application/json"
+
     def has_permissions(self, request, *args, **kwargs):
         return request.method == "POST"
-
-    def get_mimetype(self, request, *args, **kwargs):
-        return "application/json"
 
     def get_content(self, request, *args, **kwargs):
         form_id = request.POST.get("form_id")
@@ -66,3 +70,21 @@ class ApplyTransactions(WysiwygFormView):
                 Transaction(**t).apply_to(form)
             form.save()
             return form.as_json()
+
+class Edit(WysiwygFormView):
+    template_name = "wysiwyg_forms/edit.html"
+    mimetype = "text/html"
+
+    # Customize `base_template_name` to change what template
+    # `wysiwyg_forms/edit.html` will extend.
+    base_template_name = "wysiwyg_forms/base.html"
+
+    def get_context(self, request, form_id=None, *args, **kwargs):
+        context = { "base_template_name": self.base_template_name }
+        if form_id:
+            form = get_object_or_404(Form, id=form_id)
+        else:
+            form = Form.objects.create(name="New Form",
+                                       description="This is a new form.")
+        context["form"] = form.as_json()
+        return RequestContext(request, context)
