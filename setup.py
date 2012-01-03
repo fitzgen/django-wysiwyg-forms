45bcfc4b1b216fb749e3a6d77879267657ede6f6
Nick Fitzgerald <fitzgen@gmail.com>
2012-01-03 14:01:36 -0800

    Fixing up the setup.py for PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name = "Django WYSIWYG Forms",
+setup(name = "django-wysiwyg-forms",
       version = "0.1.0",
       description = "A What You See Is What You Get form editor for Django.",
       author = "Nick Fitzgerald",
       author_email = "fitzgen@gmail.com",
       url = "https://github.com/fitzgen/django-wysiwyg-forms",
       packages = find_packages(),
-      install_requires = ['django=1.3'])
+      install_requires = ['django==1.3'])
