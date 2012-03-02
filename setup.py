ca87865060f13c6a750b8555557adc9e39301f16
Nick Fitzgerald <fitzgen@gmail.com>
2012-03-01 17:43:45 -0800

    Bumping minor version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name = "django-wysiwyg-forms",
-      version = "0.1.0",
+      version = "0.1.1",
       description = "A What You See Is What You Get form editor for Django.",
       author = "Nick Fitzgerald",
       author_email = "fitzgen@gmail.com",
